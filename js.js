698a353e656a024c2af8e8c9131b64fb04af0690
Dario Martini <martinidario.cab@gmail.com>
2022-05-09 23:01:46 +0200

    javascript

--- js.js
+++ js.js
@@ -53,21 +53,21 @@ let pasaje = 0;
 let parar = 0;
 
 const movimiento = () => {
-    if (parar<=15) {
+    if (parar <= 15) {
         izquierda += velocidad;
         colectivo.style.marginLeft = izquierda + "px";
         izquierda -= velocidad;
         setTimeout(regresar, 400);
         parar++;
     }
-    else{
+    else {
         reproducir();
-        parar=0;
-        pasaje=0;
+        parar = 0;
+        pasaje = 0;
     }
-        
+
 }
- 
+
 const regresar = () => {
     derecha -= velocidad;
     colectivo.style.marginLeft = derecha + "px";
@@ -81,17 +81,109 @@ const regresar = () => {
 
 }
 
+//--------------------------------------------------------JUGADORES---------------------------------------------------------------------
 /*
-const scaloneta = () => {
-    barraBrava++
-    console.log(barraBrava);
-    if (barraBrava <= 2) {
-        movimiento();
-        }
-        if (barraBrava >=2) {
-            return
+const jugadoresElegidos = [
+    { nombre: "messi", numero: 10, puntos: document.querySelector(".ju-13").src},
+    { nombre: "aguero", numero: 11, puntos: 90 },
+    { nombre: "dibu", numero: 12, puntos: 95 },
+    { nombre: "armani", numero: 1, puntos: 75 },
+    { nombre: "otamendi", numero: 4, puntos: 90 },
+    { nombre: "romero", numero: 3, puntos: 90 },
+    { nombre: "floyth", numero: 2, puntos: 75 },
+    { nombre: "acuna", numero: 8, puntos: 80 },
+    { nombre: "correa", numero: 13, puntos: 70 },
+    { nombre: "alvarez", numero: 14, puntos: 70 },
+    { nombre: "correa1", numero: 7, puntos: 75 },
+    { nombre: "depaul", numero: 5, puntos: 85 },
+    { nombre: "tagliafico", numero: 6, puntos: 80 },
+    { nombre: "molina", numero: 15, puntos: 75 },
+    { nombre: "martinez", numero: 16, puntos: 85 },
+    { nombre: "dimaria", numero: 9, puntos: 90 },
+    { nombre: "dybala", numero: 17, puntos: 85 },
+    { nombre: "gomez", numero: 18, puntos: 75 },
+    { nombre: "rojo", numero: 19, puntos: 80 },
+    { nombre: "locelso", numero: 20, puntos: 80 },
+    { nombre: "paredes", numero: 21, puntos: 85 },
+    { nombre: "montiel", numero: 22, puntos: 80 }
+];
 
-        }
-        
+console.log(jugadoresElegidos[0].puntos);
+
+const jugadorGuardado = (nom, num) => { localStorage.setItem(nom, num) };
+jugadorGuardado("Jugadores ", JSON.stringify(jugadoresElegidos));*/
+//console.log(jugadoresElegidos[0].puntos); (este seria messi por ejemplo)
+
+/*
+const dibu = () => {  document.querySelector(".ju-1");
+let arquero1 = jugadoresElegidos[2];
+console.log(arquero1);}*/
+
+
+/*
+const messi = (hola) => { let messi = jugadoresElegidos[0]; let jugDos = messi.puntos;
+console.log(messi);*//*jug01.innerHTML = document.querySelector(".ju-1"); *//*swal("Buena elección!", "El mejor jugador del Mundo!!", "success");
+}
+
+
+let entrenador = document.querySelector(".entrenador-sel");
+const tecnico = () => {
+}
+
+*/
+//let jug01 = document.querySelector(".jugador-seleccionado1");
+
+//let sexoTecnico = prompt ("Eres Hombre o Mujer?");
+//let nombreTecnico = prompt("Como te llamas?");
+/*
+const divJugadores = document.querySelector(".jugador-seleccionado1")
+
+const seleccionar = document.querySelectorAll(".jugador");
+seleccionar.forEach((agregarJug) => {
+
+    agregarJug.addEventListener('click', agregado);
+}
+);
+function agregado (event) {
+    const boton = event.target;
+    const item = boton.closest(".jugador");
+    const itemPuntos = item.querySelector(".puntajes").innerHTML;
+    const itemImagen = item.querySelector(".ficha-jugador").src;
+   itemImagenes (itemImagen, itemPuntos);
+}
+ function itemImagenes (itemImagen, itemPuntos) {
+    const cajaJugJs = document.createElement('div')
+    const contenidoJug = 
+    `
+    <div class="lista">
+            <div class="jugadores--js">
+                <img src=${itemImagen}>
+            </div>
+            <div class="borrar--jug">
+                <button class="boton-borrar">
+                    X
+                </button>
+
+            </div>
+        </div>
+    `
+    cajaJugJs.innerHTML = contenidoJug;
+    divJugadores.append(cajaJugJs);
+
+    valorPoder (itemPuntos);
+    
+  
+  /*
+  
+  let puntosTotales = Number(itemPuntos);
+  
+  *//*
+ }
+let totalPuntos = Number (document.querySelector(".total-p").value);
+let puntosSumados = totalPuntos + totalPuntosSumados 
+ function valorPoder (itemPuntos) {
+     let total = Number(itemPuntos);
+     let totalPuntaje = totalPuntos + total;
+     console.log (totalPuntaje);
  }*/
- //--------------------------------------------------------JUGADORES---------------------------------------------------------------------
+
